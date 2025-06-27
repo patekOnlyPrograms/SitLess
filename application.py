0e4ee81731a0faa96abecde3b678447a268ddb8c
Susruth Puppala <abhirama99@live.com>
2025-06-27 22:54:09 +0100

    finished most of the backend

--- application.py
+++ application.py
@@ -1,17 +1,26 @@
+import threading
+
 from flask_bootstrap import Bootstrap5
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import asyncio
 
-
-
+from timer import convertMinutesToSeconds, countdown
 
 app = Flask(__name__)
 bootstrap = Bootstrap5(app)
-@app.route("/")
-@app.route("/index")
+@app.route("/", methods=["GET", "POST"])
+@app.route("/index", methods=["GET", "POST"])
 def main():
+    if request.method == "POST":
+        getDuration = request.form.get("duration")
+        if getDuration:
+            seconds=convertMinutesToSeconds(getDuration)
+            thread = threading.Thread(target=start_async_timer, args=(seconds,))
+            thread.start()
     return render_template('index.html')
 
-
+def start_async_timer(seconds):
+    asyncio.run(countdown(seconds))
 
 
 if __name__ == "__main__":
